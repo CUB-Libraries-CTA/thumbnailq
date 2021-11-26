fc997da1e7edd18dd3c469b534afb3d6cc4184b8
Mark Stacy <mbstacy@gmail.com>
2021-11-26 13:12:54 -0700

    pil for image RBG

--- thumbnailq/tasks/tasks.py
+++ thumbnailq/tasks/tasks.py
@@ -11,29 +11,12 @@ def imageThumbnail(bucket,key,target_fname,width=100,height=100,blob=True):
         object_content=genS3Objct(bucket,key)
         IG.MAX_IMAGE_PIXELS = None
         with IG.open(object_content) as img:
-            img.convert('RGBA')
-            img.thumbnail((width,height))
-            #img.convert('RGBA')
-            #print("1save")
-            img.save(target_fname)
-            print("1saveComplete")
+            new_img=img.convert('RGB')
+            new_img.thumbnail((width,height))
+            new_img.save(target_fname)
         return target_fname
     except Exception as e:
-        print("1save: ",str(e))
-        pass
-    try:
-        object_content=genS3Objct(bucket,key)
-        with Image(blob=object_content.read()) as img:
-            img.format = 'png'
-            img.alpha_channel = 'remove'
-            img.background_color = Color('white')
-            img.thumbnail(width,height)
-            #print("2save")
-            img.save(filename=target_fname)
-            print("2saveComplete")
-        return target_fname
-    except Exception as e1:
-        print("2save: ",str(e1))
+        #PDF attempt
         tmpfile= "{0}/file.try".format(tempfile.gettempdir())
         object_content=genS3Objct(bucket,key)
         with open(tmpfile,'wb') as f:
@@ -42,12 +25,9 @@ def imageThumbnail(bucket,key,target_fname,width=100,height=100,blob=True):
             img.format = 'png'
             img.alpha_channel = 'remove'
             img.background_color = Color('white')
-            #img.transform(resize='320x240>')
             img.thumbnail(width,height)
-            #print("3save")
             img.save(filename=target_fname)
-            print("3saveComplete")
-        return target_fname
+        pass
     return target_fname
 
 def genHash(key,split=7):
@@ -58,9 +38,8 @@ def genHash(key,split=7):
 def genS3Objct(bucket,key):
     s3_client = boto3.client('s3')
     s3_response_object = s3_client.get_object(Bucket=bucket, Key=key)
-    #object_content = s3_response_object['Body'].read()
     return s3_response_object['Body']
-2018
+
 @task()
 def generateObjectThumbnail(bucket,key,width=100,height=100,target_base='/static_secure/thumbnails'):
     """
@@ -72,21 +51,13 @@ def generateObjectThumbnail(bucket,key,width=100,height=100,target_base='/static
         hashpath=genHash(hashkey)
         thumb_filename=os.path.join(target_base,hashpath,"thumbnail.png")
         pathlib.Path(os.path.join(target_base,hashpath)).mkdir(parents=True, exist_ok=True)
-        #object_content=genS3Objct(bucket,key)
-        #try:
         imageThumbnail(bucket,key,thumb_filename,width,height)
-        #except:
-        #    imageThumbnail(object_content,thumb_filename,width,height,blob=False)
-        #object_content=None
         result={"key":key,"thumbnail":thumb_filename}
     except Exception as e:
-        print("3save: ",str(e))
         thumb_filename=os.path.join(target_base,hashpath,"thumbnail.png")
-        #src_default=os.path.join(pathlib.Path().resolve(),"thumbnailq/tasks","files/default-thumbnail.png")
         src_default=os.path.join(pathlib.Path(__file__).parent.resolve(),"files/default-thumbnail.png")
         pathlib.Path(os.path.join(target_base,hashpath)).mkdir(parents=True, exist_ok=True)
         copyfile(src_default,thumb_filename)
-        print("4saveComplete")
         result={"key":key,"thumbnail":thumb_filename}
         pass
     return result
